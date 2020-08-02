ae1fe85edcbdf4577c15377ef02f3a8e0dda3257
Maria Afanaseva <mashenkachukina@gmail.com>
2020-08-02 20:49:54 +0200

    Created project on Vue CLI (bike_store), added home, catalog, contacts pages.

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="app" class="container">
+        <div class="top">
+            <div class="header">
+                <router-link to="/">
+                    <img class="logo" src="./assets/foto/logo.jpg" alt="Logo BikeStore">
+                </router-link>
+                    <ul class="menu">
+                        <li class="menu_list">
+                            <router-link class="menu_link" to="/">Home</router-link>
+                        </li>
+                        <li class="menu_list">
+                            <router-link class="menu_link" to="/catalog">Catalog</router-link>
+                        </li>
+                        <li class="menu_list">
+                            <router-link class="menu_link" to="/contacts">Contacts</router-link>
+                        </li>
+                    </ul>
+                </div>
+            <router-view/>
+        </div>
+        <div class="footer">
+            <p class="footer_text">&copy;&nbsp;All rights reserved</p>
+        </div>
+    </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="content">
+        <h1 class="title">BikeStore</h1>
+        <img class="index_img_title"
+        src="../assets/foto/title.jpg" alt="Mountain Bike">
+        <p class="index_text">
+            Welcome to the modern bike shop.
+            Here you will find a bike to your liking.
+        </p>
+    </div>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'Home',
+// components: {
+// HelloWorld,
+// },
+};
+</script>
